0938ceae7e0225f46996ed56ee3750ccb6e18ecf
Vu The Duc <the-duc.vu@etu.univ-orleans.fr>
2025-01-08 23:25:34 +0100

    day 2

---
+++ A*.py
@@ -0,0 +1,50 @@
+import heapq
+
+def a_start_search(graph, start, end):
+
+    def heuristique(node, end):
+        return abs(node[0] - end[0]) + abs(node[1]-end[1])
+    
+    open_set = []
+    heapq.heappush(open_set, (0, start))
+    came_from = {}
+    g_score = {start: 0}
+    f_score = {start: heuristique(start, end)}
+
+    while open_set:
+        _, current = heapq.heappop(open_set)
+
+        if current == end:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            return path[::-1]
+        
+        for neighbor, cost in graph[current]:
+            tentative_g_score = g_score[current] + cost
+            if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = tentative_g_score + heuristique(neighbor, end)
+                heapq.heappush(open_set, (f_score[neighbor], neighbor))
+
+    return None
+
+
+if __name__ == "__main__":
+
+    graph = {
+    (0, 0): [((0, 1), 1), ((1, 0), 1)],
+    (0, 1): [((0, 0), 1), ((1, 1), 1), ((0, 2), 1)],
+    (0, 2): [((0, 1), 1), ((1, 2), 1)],
+    (1, 0): [((0, 0), 1), ((1, 1), 1)],
+    (1, 1): [((1, 0), 1), ((0, 1), 1), ((1, 2), 1)],
+    (1, 2): [((1, 1), 1), ((0, 2), 1)]
+}
+    
+    start = (0, 0)
+    end = (1, 2)
+    path = a_start_search(graph, start, end)
+    print("Le chemin le plus court est: ", path)
